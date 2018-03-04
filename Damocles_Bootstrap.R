2d6d051a74df3f5362b70f5c1bb35394eb1409fb
Jesus N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2018-03-04 11:10:52 -0600

    Update Damocles_Bootstrap.R

--- Damocles_Bootstrap.R
+++ Damocles_Bootstrap.R
@@ -8,15 +8,15 @@ library("snow")
 source("https://raw.githubusercontent.com/csdambros/R-functions/master/passSSHid.R")
 
 passSSHid()
-
+# Load your community dataset
 comms <- read.table("furnariides_PA_allnew.txt", header = T, row.names = 1)
-
+# Load your phylogeny
 tree <- read.nexus("furna_divTime_WO_MCC_newNames.nex")
-
+# Match the community dataset with your phylogeny
 matched2 <- match.phylo.comm(phy = tree, comm = t(comms))
 
 
-##### Function to run in parallel
+##### Function to run in parallel #####
 
 DemonDAMOCLES <- function(tree, comm, initparsopt, idparsopt, parsfix, idparsfix, 
                          pars2, pchoice, runs, estimate_pars, conf.int){
@@ -41,6 +41,7 @@ DemonDAMOCLES <- function(tree, comm, initparsopt, idparsopt, parsfix, idparsfix
 }
 }
 
+# Run Damocles model using the matched information
 BootsDAMAll <- DemonDAMOCLES(tree = matched2[[1]], comm = matched2[[2]], initparsopt = c(0.05, 0.05), 
                   parsfix = c(0), idparsfix = c(3),
                   idparsopt = c(1, 2), pars2 = c(1E-3, 1E-4, 1E-5, 10000), pchoice = 0,
